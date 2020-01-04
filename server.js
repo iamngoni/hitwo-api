1cd991ddb62759a46e6af5f36c1ec4979597ba99
iamngvj <el.nick.ngonie@gmail.com>
2020-01-04 23:37:04 +0200

    test

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const config = require('./config');
 const app = require('./app');
 const db = require('./db');
 
+console.log(config.env);
+
 db().then(dbconnection => {
     console.log('db connected');
     console.log(config.message);
