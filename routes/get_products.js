61e59bda1fbc7fa4760447ff46070c2aa1ef8d3a
iamngvj <el.nick.ngonie@gmail.com>
2020-01-10 05:15:02 +0200

    err handling

--- routes/get_products.js
+++ routes/get_products.js
@@ -10,7 +10,7 @@ router.get('/', (req, res) => {
                 });
             }
             res.status(200).json(data);
-        });
+        }).catch(err => console.log(err));
 });
 
 module.exports = router;
