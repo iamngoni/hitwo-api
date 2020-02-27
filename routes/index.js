68fd508792147b846001fe98a75345e9bfa9d48b
iamngvj <el.nick.ngonie@gmail.com>
2020-02-27 22:00:43 +0200

    f

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 router.use('/signup', require('./mobile/signup'));
 router.use('/signin', require('./mobile/signin'));
-router.use('/otp', require('./auth/mobile-login'));
+router.use('/otp', require('./mobile/mobile-login'));
 router.use('/upload', require('./upload'));
 router.use('/products', require('./utilities/get_products'));
 router.use('/store/signup', require('./web/signup'));
