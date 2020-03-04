0d4c44aed45dedd8dcf5ef36455ecf78bda8590b
iamngvj <el.nick.ngonie@gmail.com>
2020-03-04 20:42:12 +0200

    adding search functionality

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ router.use('/signup', require('./mobile/signup'));
 router.use('/signin', require('./mobile/signin'));
 router.use('/otp', require('./mobile/mobile-login'));
 router.use('/upload', require('./upload'));
-router.use('/products', require('./utilities/get_products'));
+router.use('/products', require('./mobile/fetch'));
 router.use('/store/signup', require('./web/signup'));
 router.use('/store/signin', require('./web/signin'));
 
