6ba1666fb0282a65022f0976ce465311fbf10648
iamNGVJ <el.nick.ngonie@gmail.com>
2020-03-17 23:23:21 +0200

    changed fetch

--- routes/mobile/fetch.js
+++ routes/mobile/fetch.js
@@ -3,7 +3,7 @@ const Products = require('./../../models/products');
 
 router.get('/', (req, res) => {
     Products.find().then((data) => {
-        res.status(200).json({data});
+        res.status(200).send(data);
     });
 });
 
