7d6f5e0e53f4a204520c470a8e0ea3cfae3476fe
iamngoni <el.nick.ngonie@gmail.com>
2020-07-04 09:47:47 +0200

    undefined

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -2,12 +2,12 @@ const jwt = require('jsonwebtoken')
 
 module.exports = {
   verifyToken: async (req, res, next) => {
-    const bearerHeader = req.headers.Authorization
+    const bearerHeader = req.headers.authorization
     if (typeof bearerHeader !== 'undefined') {
       const bearer = bearerHeader.split(' ')
       const token = bearer[1]
       if (!token) {
-        return res.status(403).json({})
+        return res.status(403).json({message: "no token found"})
       }
       try {
         const decoded = await jwt.verify(token, 'hitwo-api')
@@ -17,7 +17,7 @@ module.exports = {
         return res.status(500).json({})
       }
     } else {
-      return res.status(403).json({})
+      return res.status(403).json({message: "bearer is undefined"})
     }
   }
 }
